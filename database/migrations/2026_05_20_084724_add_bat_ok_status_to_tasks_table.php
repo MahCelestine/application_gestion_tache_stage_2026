a9096e7d40808b066c23ef25ea51a87ba218f52e
Morgan <morgan@iMac-de-Morgan-2.local>
2026-06-10 14:53:32 +0200

    date des migration

--- database/migrations/2026_05_20_084724_add_bat_ok_status_to_tasks_table.php
+++ database/migrations/2026_05_20_084724_add_bat_ok_status_to_tasks_table.php
@@ -12,15 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-<<<<<<< HEAD
-            $table->enum('status', ['en cours', 'validé', 'bloqué', 'BAT ok', 'attente BAT'])
-                ->default('en cours')
-                ->change();
-=======
             $table->enum('status', ['en cours', 'validé', 'bloqué', 'attente BAT', 'BAT ok'])
               ->default('en cours')
               ->change();
->>>>>>> 751bd0e07325174818002f5f148742af5a262319
         });
     }
 
@@ -30,13 +24,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-<<<<<<< HEAD
-            //
-=======
             $table->enum('status', ['en cours', 'validé', 'bloqué', 'attente BAT', 'BAT ok'])
               ->default('en cours')
               ->change();
->>>>>>> 751bd0e07325174818002f5f148742af5a262319
         });
     }
 };
--- database/migrations/2026_05_20_084738_add_bat_ok_status_to_subtasks_table.php
+++ database/migrations/2026_05_20_084738_add_bat_ok_status_to_subtasks_table.php
@@ -4,23 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-<<<<<<< HEAD
-return new class extends Migration
-{
-=======
 return new class extends Migration {
->>>>>>> 751bd0e07325174818002f5f148742af5a262319
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('subtasks', function (Blueprint $table) {
-<<<<<<< HEAD
-            $table->enum('status', ['en cours', 'validé', 'bloqué', 'BAT ok', 'attente BAT'])
-=======
             $table->enum('status', ['en cours', 'validé', 'bloqué', 'attente BAT', 'BAT ok'])
->>>>>>> 751bd0e07325174818002f5f148742af5a262319
                 ->default('en cours')
                 ->change();
         });
